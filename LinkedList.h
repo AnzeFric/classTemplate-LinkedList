97cf7cc7fc72a4a6948e0eefa464efefb453f0cf
AnzeFric <120261988+AnzeFric@users.noreply.github.com>
2023-05-13 10:29:31 +0200

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -11,75 +11,83 @@
 template<typename T>
 class LinkedList {
 private:
-    Node<T>* head = new Node<T>(-1);
+    Node<T> *head = new Node<T>(-1);
 public:
-    bool isEmpty() const;
-    int getSize() const;
-    void add(T value);
-    T at(unsigned int index);
-    ~LinkedList();
-};
+    bool isEmpty() const {
+        return (LinkedList<T>::head == nullptr);
+    };
 
-template<typename T>
-bool LinkedList<T>::isEmpty() const {
-    if(LinkedList<T>::head->getNext() == nullptr){
-        return true;
-    }
-    return false;
-}
+    int getSize() const {
+        Node<T> *it = head;
+        int counter = 0;
 
-template<typename T>
-int LinkedList<T>::getSize() const {
-    Node<T>* it = head;
-    int counter = 0;
-
-    while (it->getNext() != nullptr){
-        counter++;
-        it = it->getNext();
-    }
-
-    return counter;
-}
-
-template<typename T>
-void LinkedList<T>::add(T value) {
-    Node<T>* new_node = new Node<T>(value);
-    Node<T>* it = head;
-
-    while(it->getNext() != nullptr){
-        it = it->getNext();
-    }
-
-    it->setNext(new_node);
-}
-
-template<typename T>
-T LinkedList<T>::at(unsigned int index) {
-    Node<T>* it = head;
-    int counter = 0;
-
-    while (it != nullptr) {
-        if (counter == index) {
-            return (it->getValue());
+        while (it->getNext() != nullptr) {
+            counter++;
+            it = it->getNext();
         }
 
-        counter++;
-        it = it->getNext();
-    }
+        return counter;
+    };
 
-    return -1;
-}
+    void add(T value) {
+        Node<T> *new_node = new Node<T>(value);
+        Node<T> *it = head;
 
-template<typename T>
-LinkedList<T>::~LinkedList() {
-    Node<T>* it = head;
+        while (it->getNext() != nullptr) {
+            it = it->getNext();
+        }
 
-    while(it != nullptr) {
-        Node<T>* temp = it;
-        it = it->getNext();
-        delete temp;
-    }
-}
+        it->setNext(new_node);
+    };
+
+    T at(unsigned int index) {
+        Node<T> *it = head;
+        int counter = 0;
+
+        while (it != nullptr) {
+            if (counter == index) {
+                return (it->getValue());
+            }
+
+            counter++;
+            it = it->getNext();
+        }
+
+        return -1;
+    };
+
+    bool remove(unsigned int index) {
+        if (LinkedList<T>::isEmpty() || index <= 0) {
+            return false;
+        }
+
+        Node<T> *previous = nullptr;
+        Node<T> *it = head;
+
+        for (int i = 0; it != nullptr && i != index; i++) {
+            previous = it;
+            it = it->getNext();
+        }
+
+        if (it == nullptr) {
+            return false;
+        }
+
+        previous->setNext(it->getNext());
+        delete it;
+        return true;
+    };
+
+    ~LinkedList() {
+        Node<T> *it = head;
+
+        while (it != nullptr) {
+            Node<T> *temp = it;
+            it = it->getNext();
+            delete temp;
+        }
+    };
+};
 
 
 #endif //NALOGA0802_LINKEDLIST_H
